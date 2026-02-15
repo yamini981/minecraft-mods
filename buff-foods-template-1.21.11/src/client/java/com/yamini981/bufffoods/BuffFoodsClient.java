2f72a6a6d33f1360f10a945a517fdc7753a8debf
Mustafa Butt <coolmustafabutt9@gmail.com>
2026-02-15 16:01:24 -0500

    buff foods mod template add

---
+++ buff-foods-template-1.21.11/src/client/java/com/yamini981/bufffoods/BuffFoodsClient.java
@@ -0,0 +1,10 @@
+package com.yamini981.bufffoods;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BuffFoodsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ buff-foods-template-1.21.11/src/client/java/com/yamini981/bufffoods/BuffFoodsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.yamini981.bufffoods;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BuffFoodsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
